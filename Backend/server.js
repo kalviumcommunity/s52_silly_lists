e3312634a17ff8d07b9778ab546314959a257a79
santhosh-005 <santhoshdon000555@gmail.com>
2024-02-05 11:24:57 +0530

    DB connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const app = express()
 const PORT = 3000
 
-app.get('/ping',(req,res)=>{
-    res.send('<h1>On ping route</h1>')
+app.get('/',async (req,res)=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        res.status(200).send('<h1>DB Connected</h1>')
+    }catch(error){
+        res.status(400).send('<h1>DB Connection Failed</h1>')
+    }
 })
 
 app.listen(PORT,()=>{
