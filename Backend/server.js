d7516ad0afaf7110a541d156c4c3962ad813e27a
santhosh-005 <santhoshdon000555@gmail.com>
2024-02-28 13:43:16 +0530

    cors-added

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000
 
 app.use(cookiParser())
 
-app.use(cors({ origin: ['http://localhost:5173','https://listy-lists.netlify.app/'], credentials: true }));
+app.use(cors())
+app.use(cors({ origin: ['https://listy-lists.netlify.app/','http://localhost:5173'], credentials: true }));
 
 app.use(async (req, res, next) => {
     try{
